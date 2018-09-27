a6e0c8afea196977143871d737d7bc12b53328f3
Phil Nash <philnash@gmail.com>
2018-09-27 14:39:51 +1000

    Adds proxied server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+);
