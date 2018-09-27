665809cb9eabe8c51086794e56f6e9ca41fb1019
Phil Nash <philnash@gmail.com>
2018-09-27 17:04:57 +1000

    Adds pino for server logging

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
 
 app.get('/api/greeting', (req, res) => {
   const name = req.query.name || 'World';
