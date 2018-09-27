950845861dfa4e2faf8a92c9ba2d9e90592bd6b6
Phil Nash <philnash@gmail.com>
2018-09-27 16:09:02 +1000

    Uses the proxy API from the front end.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      greeting: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+      .then(response => response.json())
+      .then(state => this.setState(state));
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +34,17 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Enter your name: </label>
+          <input
+            id="name"
+            type="text"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.greeting}</p>
       </div>
     );
   }
