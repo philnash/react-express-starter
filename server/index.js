8a181e168b81b8c5fa02482f749d09da587fc591
Harshit Singhal <33669026+123Harshit@users.noreply.github.com>
2020-10-20 17:11:57 +0530

    added twilio at backend and made an api

--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
-
+const client = require('twilio')(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(pino);
+
+app.post('/api/messages', (req, res) => {
+  res.header('Content-Type', 'application/json');
+  client.messages
+    .create({
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: req.body.to,
+      body: req.body.body
+    })
+    .then(() => {
+      res.send(JSON.stringify({ success: true }));
+    })
+    .catch(err => {
+      console.log(err);
+      res.send(JSON.stringify({ success: false }));
+    });
+});
 
 app.get('/api/greeting', (req, res) => {
   const name = req.query.name || 'World';
