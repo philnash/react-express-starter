a66db27374a4749f5fd37676797ee3e9fe0bb2a3
Crystal Korth <101788833+defworm@users.noreply.github.com>
2022-12-11 19:57:12 -0600

    updated index.html to remove navbar and footer as these will become components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+
 
 class App extends Component {
   constructor(props) {
