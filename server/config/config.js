56d148721dd07635637b279b44c5e33c8482c53f
cre8ivesolutions <tammerz78@gmail.com>
2022-12-12 15:14:00 -0600

    updated variables in config.js file

--- server/config/config.js
+++ server/config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config()
 
 module.exports = {
     "development": {
-      "username": process.env.DB_USERNAME,
-      "password": null,
-      "database": "firestarter",
-      "host": "127.0.0.1",
-      "dialect": "postgres"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": "127.0.0.1",
+    "dialect": "postgres"
     },
     "test": {
-      "username": process.env.DB_USERNAME,
-      "password": null,
-      "database": "firestarter",
-      "host": "127.0.0.1",
-      "dialect": "postgres"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": "127.0.0.1",
+    "dialect": "postgres"
     },
     "production": {
-      "username": process.env.DB_USERNAME,
-      "password": null,
-      "database": "firestarter",
-      "host": "127.0.0.1",
-      "dialect": "postgres"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": "127.0.0.1",
+    "dialect": "postgres"
     }
   }
