4101663933b86a7b2f87a3dba1047a22aec988d8
cre8ivesolutions <tammerz78@gmail.com>
2022-12-13 11:56:13 -0600

    merged

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 <<<<<<< Updated upstream
-import React, { Component } from 'react';
+import * as React from "react";
+import { Component } from 'react';
 import logo from '../src/flame.png';
 // import './App.css';
 
