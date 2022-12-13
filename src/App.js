a58039dd0cd00967678d396abeca992b6e28464b
Crystal Korth <101788833+defworm@users.noreply.github.com>
2022-12-12 21:52:35 -0600

    working on relocating css and scss files to update app.css correctly. Also just starting on hero component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import logo from '../src/flame.png';
+// import './App.css';
+
+// I NEED TO IMPORT PUBLIC/CSS/main.css HERE since SASS is updating that right now, App.css is not auto-updating at this time. 
 
 
 
@@ -30,29 +32,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="name">Enter your name: </label>
-            <input
-              id="name"
-              type="text"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <p>{this.state.greeting}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
+          
+          
+        
+         
         </header>
       </div>
     );
