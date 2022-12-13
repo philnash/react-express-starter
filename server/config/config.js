1c682adb98a4b39c458758ac8e5bb116049a92f6
cre8ivesolutions <tammerz78@gmail.com>
2022-12-12 21:22:12 -0600

    starting migrations

--- server/config/config.js
+++ server/config/config.js
@@ -4,9 +4,9 @@ console.log(process.env.DB_PASSWORD)
 
 module.exports = {
     "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
+    "username": "postgres",
+    "password": "password",
+    "database": "firestarter",
     "host": "127.0.0.1",
     "dialect": "postgres"
     },
