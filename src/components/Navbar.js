84922c3990c3df27a6f1c7a29e6debabe9530e40
Cody Cork <codycork.sfg@outlook.com>
2022-12-14 21:36:51 -0600

    merge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,42 @@
 import React from "react";
 import { Nav, NavLink, Navbar } from "react-bootstrap";
-// export const NavBar = Navbar
+// import {} from "";
 
-function Navigation(){
+function Navigation() {
   return (
-    <div>
+    <div className="Navbar">
       <Nav>
         <Navbar>
-          <NavLink href="/home" to="./pages/home"><i class="fa-solid fa-house"></i>
+          <NavLink href="/home" to="./pages/home">
+            <i class="fa-solid fa-house"></i>
           </NavLink>
           <NavLink href="/products" to="./pages/products">
             Products
           </NavLink>
           <NavLink href="/aboutus" to="./pages/about_us">
-            About Us 
+            About Us
           </NavLink>
           {/* I added the fontawesome icons for fun. Feel free to remove them or use them */}
-           <NavLink href="/profile"><i class="fa-regular fa-circle-user"></i></NavLink>
-           <button><NavLink href="/login" to="./pages/login">
-             Login
-          </NavLink></button>
-          <button><NavLink href="/createuser" to="./pages/createuser">
-            Create an Account
-          </NavLink></button>
-         
-          
-          <NavLink href="/checkout"><i class="fa fa-shopping-cart" aria-hidden="true"></i></NavLink>
-
-         
+          <NavLink href="/profile">
+            <i class="fa-regular fa-circle-user"></i>
+          </NavLink>
+          <button>
+            <NavLink href="/login" to="./pages/login">
+              Login
+            </NavLink>
+          </button>
+          <button>
+            <NavLink href="/createuser" to="./pages/createuser">
+              Create an Account
+            </NavLink>
+          </button>
+          <NavLink href="/checkout">
+            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+          </NavLink>
         </Navbar>
       </Nav>
     </div>
   );
-};
+}
 
 export default Navigation;
