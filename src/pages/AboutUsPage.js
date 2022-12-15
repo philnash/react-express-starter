f839503e567d0721af7aa03da41de473899b7741
Crystal Korth <101788833+defworm@users.noreply.github.com>
2022-12-14 19:44:33 -0600

    Revert "testing a few things, not looking to merge yet"
    This reverts commit 4cb6f2c264c48197c1e3672477001112c7672649.

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,31 +1,9 @@
-import Footer from "../components/Footer";
-
-
 function AboutUsPage() {
     return (
-      <>
-    
-    <main>
-    <div>
-      
-    </div>
-        <div class="home">
-          <h2>Welcome to Our Site!</h2>
-          <h1 class="home__name">
-            THE 1 <span class="home__name--last">Fire Starter</span>
-          </h1>
-          <h2>Light Up Your Life</h2>
-
-          <hr />
-          <br />
-          
-          <Footer />
-        </div>
-        <div>
-        
-        </div>
-      </main></>
-  );
-}
+      <main>
+        <h1>AboutUsPage</h1>
+      </main>
+    );
+  }
   
   export default AboutUsPage
