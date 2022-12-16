0748c4929b422ec8bbafc87b3ac9baf256f53c18
Crystal Korth <101788833+defworm@users.noreply.github.com>
2022-12-16 06:38:45 -0600

    fixing conflicts in merge request

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,44 @@
 import React from "react";
 import { Nav, NavLink, Navbar } from "react-bootstrap";
-// export const NavBar = Navbar
+import Container from "react-bootstrap/Container";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { LinkContainer } from "react-router-bootstrap";
 
-function Navigation(){
+function Navigation() {
   return (
-    <div>
+    <Navbar className="Navbar">
       <Nav>
         <Navbar>
           <NavLink href="/home" to="./pages/home">
-            <i class="fa-solid fa-house"></i>
+            <i className="fa-solid fa-house"></i>
           </NavLink>
           <NavLink href="/products" to="./pages/products">
             Products
           </NavLink>
           <NavLink href="/aboutus" to="./pages/about_us">
-            About Us 
+            About Us
           </NavLink>
           {/* I added the fontawesome icons for fun. Feel free to remove them or use them */}
-           <NavLink href="/profile"><i class="fa-regular fa-circle-user"></i></NavLink>
-           <button><NavLink href="/login" to="./pages/login">
-             Login
-          </NavLink></button>
-          <button><NavLink href="/createuser" to="./pages/createuser">
-            Create an Account
-          </NavLink></button>
-         
-          
-          <NavLink href="/checkout"><i class="fa fa-shopping-cart" aria-hidden="true"></i></NavLink>
-
-         
+          <NavLink href="/profile">
+            <i className="fa-regular fa-circle-user"></i>
+          </NavLink>
+          <button>
+            <NavLink href="/login" to="./pages/login">
+              Login
+            </NavLink>
+          </button>
+          <button>
+            <NavLink href="/createuser" to="./pages/createuser">
+              Create an Account
+            </NavLink>
+          </button>
+          <NavLink href="/checkout">
+            <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+          </NavLink>
         </Navbar>
       </Nav>
-    </div>
+    </Navbar>
   );
-};
+}
 
 export default Navigation;
