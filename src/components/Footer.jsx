22d6c184e1ac1e4e79c5ee85c1b900546c6578d0
Crystal Korth <101788833+defworm@users.noreply.github.com>
2022-12-17 12:36:09 -0600

    move footer outside of header app.js

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ function Footer(){
     return (
       <div className="footer">
        <h3>&copy; Copyright 2022: THE 1 Fire Starter, LLC</h3>
+       
         
         <p><a href="email@email.com"><i class="fa-solid fa-envelope"></i></a></p>
         <i class="fa-sharp fa-solid fa-fire-flame"></i>
